77bb5c0fc1dacb780f652b41c829cdb41026dcfa
dharmakk <vjsdharma@gmail.com>
2025-03-16 22:44:19 +0530

    Create Myfirstprogram.java

---
+++ Myfirstprogram.java
@@ -0,0 +1,10 @@
+public class Myfirstprogram
+{
+public static void main(String[] args){
+int a=10;
+int b=20;
+int c=a+b;
+String s="Addition";
+System.out.println(s+" "+"of two numbers:"+ " "+c);
+}
+}
